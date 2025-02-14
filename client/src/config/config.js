fa61968a5d8a1b1b78a5aa2dc688b63c7f77ead1
aizaz <aizaz060198@gmail.com>
2025-02-14 12:55:53 +0530

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:5000";
-// const URL = "";
+// const URL = "http://localhost:5000"
+const URL = "https://reactvideocall-f09j.onrender.com";
 
 export const socket = io(URL);
 export const navbarBrand = "Video Call App";
